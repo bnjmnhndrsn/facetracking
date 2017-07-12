a1cb70cbb5f4a1a3a1728c57b48b127944fb93ac
Ben Henderson <ben@nestio.com>
2017-07-11 23:23:48 -0700

    get it rendering stuff

--- src/index.js
+++ src/index.js
@@ -1,92 +1,99 @@
-let App = {
-    timerCallback: function() {
-        if (this.video.paused || this.video.ended) {
-            this.idx = 0;
-            this.playbackCallback();
-            return;
-        }
+class LoadingView {
+    constructor({videoUrl}){
+        this.videoUrl = videoUrl;
+        this.frames = [];
+    }
 
-        this.computeFrame();
-        let self = this;
-        setTimeout(function () {
-            self.timerCallback();
-        }, 100);
-    },
+    render(){
+        this.el = document.createElement('div');
 
-    playbackCallback: function(){
-        var frame = this.finished[this.idx];
-        if (!frame) {
-            return;
-        }
-        this.idx++;
-        this.drawImage(frame);
-        var self = this;
-        setTimeout(function () {
-            self.playbackCallback();
-        }, 100);
-    },
+        this.el.innerHTML = `
+            <div>\
+            <video autoplay muted style="display:none;"><source src="${this.videoUrl}"/></video>\
+            <canvas style="display:none;" id="blitCanvas"></canvas>\
+            <canvas id="displayCanvas"></canvas> \
+            <button id="stop">Stop</button> \
+            </div>\
+        `;
 
-    init: function() {
-        this.video = document.getElementById("video");
-        let self = this;
+        return this.el;
 
-        this.tracker = new tracking.ObjectTracker('face');
-        this.canvas1 = document.getElementById("canvas1");
-        this.ctx1 = this.canvas1.getContext("2d");
+    }
 
-        this.canvas2 = document.getElementById("canvas2");
-        this.ctx2 = this.canvas2.getContext("2d");
+    onRender(){
+        this.video = this.el.querySelector('video');
+        this.blitCanvas = this.el.querySelector('#blitCanvas');
+        this.blitCtx = this.blitCanvas.getContext("2d");
+        this.displayCanvas = this.el.querySelector('#displayCanvas');
+        this.displayCtx = this.displayCanvas.getContext("2d");
+        this.button = this.el.querySelector('#stop');
 
-        this.video.addEventListener("play", function() {
-            self.width = self.video.width;
-            self.height = self.video.height;
-            self.timerCallback();
+        this.video.addEventListener("play", () => {
+            this.videoWidth = this.video.videoWidth;
+            this.videoHeight  = this.video.videoHeight;
+            this.blitCanvas.style.height = `${this.videoHeight}px`;
+            this.blitCanvas.style.width = `${this.videoWidth}px`;
+            this.displayCanvas.style.height = `${this.videoHeight}px`;
+            this.displayCanvas.style.width = `${this.videoWidth}px`;
+            this.startCapturing();
         }, false);
 
-        this.queue = [];
-        this.finished = [];
-    },
+        this.video.addEventListener("end", () => {
+            this.stopCapturing();
+        }, false);
 
-    tryTracking: function(){
-        if (this.isTracking || !this.queue.length) {
+        this.button.addEventListener('click', (e) => {
+            this.stopCapturing();
+        }, false);
+    }
+
+    startCapturing(){
+        this._isCapturing = true;
+        this.captureFrame();
+    }
+
+    captureFrame(){
+        if (!this._isCapturing) {
             return;
         }
 
-        this.isTracking = true;
-        let self = this;
-        this.tracker.once('track', function(event) {
-            self.finished.push({
-                data: event.data,
-                image: frame
-            });
-            self.isTracking = false;
-            self.tryTracking();
+        this.blitCtx.drawImage(this.video, 0, 0, this.videoWidth, this.videoHeight);
+        const frame = this.blitCtx.getImageData(0, 0, this.videoWidth, this.videoHeight);
+        this.frames.push(frame);
+        this.drawImage(frame);
+
+        requestAnimationFrame(() => {
+            this.captureFrame();
         });
-        var frame = this.queue.shift();
-        this.tracker.track(frame.data, this.width, this.height);
-    },
-    computeFrame: function() {
-        this.ctx1.drawImage(this.video, 0, 0, this.width, this.height);
-        let frame = this.ctx1.getImageData(0, 0, this.width, this.height);
-        this.queue.push(frame);
-        this.tryTracking();
-    },
-    drawImage: function(frame){
-        var self = this;
-        self.ctx2.clearRect(0, 0, self.canvas2.width, self.canvas2.height);
-        self.ctx2.putImageData(frame.image, 0, 0);
-        frame.data.forEach(function(rect) {
-            var x = rect.x + rect.width / 2;
-            var y = rect.y + rect.height / 2;
-            self.ctx2.beginPath();
-            self.ctx2.arc(x, y, 5, 0, 2 * Math.PI, false);
-            self.ctx2.fillStyle = 'green';
-            self.ctx2.fill();
-            self.ctx2.lineWidth = 5;
-            self.ctx2.strokeStyle = '#003300';
-            self.ctx2.stroke();
-        });
+
+    }
+
+    stopCapturing(){
+        this._isCapturing = false;
+    }
+
+    drawImage(frame){
+        this.displayCtx.clearRect(0, 0, this.displayCanvas.width, this.displayCanvas.height);
+        this.displayCtx.putImageData(frame, 0, 0);
+        // frame.data.forEach(function(rect) {
+        //     var x = rect.x + rect.width / 2;
+        //     var y = rect.y + rect.height / 2;
+        //     self.ctx2.beginPath();
+        //     self.ctx2.arc(x, y, 5, 0, 2 * Math.PI, false);
+        //     self.ctx2.fillStyle = 'green';
+        //     self.ctx2.fill();
+        //     self.ctx2.lineWidth = 5;
+        //     self.ctx2.strokeStyle = '#003300';
+        //     self.ctx2.stroke();
+        // });
     }
 };
 
-App.init();
+const App = {
+    init: function(){
+        const container = document.getElementById('container');
+        const view = new LoadingView({videoUrl: 'assets/video.ogv'});
+        container.appendChild(view.render());
+        view.onRender();
+    }
+}
